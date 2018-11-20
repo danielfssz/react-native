85e591c6710865b5160ed6935683182c22e45fd6
daniel souza <danielfss98@gmail.com>
2018-11-20 16:32:47 -0200

    First commit + customização de botões

---
+++ index.js
@@ -0,0 +1,38 @@
+import React from "react";
+import { Text, View, AppRegistry, TouchableOpacity } from "react-native";
+
+const Estilos = {
+  principal: {
+    paddingTop: 40
+  },
+  botao: {
+    backgroundColor: '#48BBEC',
+    padding: 10,
+    borderColor: '#1d8eb8',
+    borderWidth: 3,
+    borderRadius: 8
+  }, 
+  textoBotao:{
+    color: '#fff',
+    fontSize: 16,
+    fontWeight: 'bold',
+    alignSelf: 'center'
+  }
+};
+
+const botaoPressionado = () => {
+  alert('Botao Pressionado');
+}
+
+const App = () => {
+  const { principal, botao, textoBotao } = Estilos;
+  return (
+    <View style={principal}>
+      <TouchableOpacity style={botao}>
+        <Text style={textoBotao}>Clique aqui</Text>
+      </TouchableOpacity>
+    </View>
+  );
+};
+
+AppRegistry.registerComponent('app2', () => App);
