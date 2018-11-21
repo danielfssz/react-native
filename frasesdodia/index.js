e988f73ecc6540ac8c044903554c8717b6d8e26c
Daniel Souza <danielfss98@gmail.com>
2018-11-21 10:44:09 -0200

    Implementação da logica

--- frasesdodia/index.js
+++ frasesdodia/index.js
@@ -20,9 +20,18 @@ const Estilos = {
     }
 }
 
-// const botaoPressionado = () => {
+const gerarFrase = () => {
+    var nAleatorio = Math.floor(Math.random() * 5);
 
-// }
+    var frases = Array();
+    frases[0] = '"Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci elit..."';
+    frases[1] = 'Lorem ipsum habitasse etiam enim pellentesque, quisque erat curae nullam magna, interdum vestibulum ullamcorper tempus.';
+    frases[2] = 'Lorem ipsum habitasse dictum faucibus gravida, porta mauris in congue consectetur, adipiscing quis donec placerat. ';
+    frases[3] = 'Lorem ipsum sociosqu molestie dictumst donec, a eu tellus aenean odio, curabitur sodales curabitur felis. ';
+    frases[4] = 'Donec leo vivamus curabitur dictum pharetra netus dictum torquent, posuere vivamus amet quisque inceptos id. ';
+
+    alert(frases[nAleatorio]);    
+}
 
 const App = () => {
     const { principal, botao, textoBotao } = Estilos;
@@ -32,7 +41,9 @@ const App = () => {
             <Image
                 source={require('./imgs/logo.png')}
             />
-            <TouchableOpacity style={botao}>
+            <TouchableOpacity 
+            onPress={gerarFrase}
+            style={botao}>
                 <Text style={textoBotao}>Nova frase...</Text>
             </TouchableOpacity>
         </View>
