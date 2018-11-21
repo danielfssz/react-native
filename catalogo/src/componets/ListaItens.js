2b3eb7b2fb4459a20a7e1bb8d60d44b412898328
Daniel Souza <danielfss98@gmail.com>
2018-11-21 16:38:59 -0200

    Axios + consumindo api +  console log (data)

--- catalogo/src/componets/ListaItens.js
+++ catalogo/src/componets/ListaItens.js
@@ -1,8 +1,21 @@
 import React, { Component } from "react";
 import { View } from "react-native";
 import Item from "../componets/Item";
+import axios from "axios";
 
 export default class catologo extends Component {
+  componentWillMount() {
+    //Requisição http
+    axios
+      .get("http://faus.com.br/recursos/c/dmairr/api/itens.html")
+      .then(res => {
+        console.log(res);
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  }
+
   render() {
     return (
       <View>
