4875891659d160dd0dff419fba69cc5e3b0f2f7d
Daniel Souza <danielfss98@gmail.com>
2018-11-21 10:35:03 -0200

    Aplicando estilos a tela

--- frasesdodia/index.js
+++ frasesdodia/index.js
@@ -1,7 +1,43 @@
-/** @format */
+import React from "react";
+import { AppRegistry, View, Image, TouchableOpacity, Text } from "react-native";
 
-import {AppRegistry} from 'react-native';
-import App from './App';
-import {name as appName} from './app.json';
+const Estilos = {
+    principal: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center'
+    },
+    botao: {
+        backgroundColor: '#538530',
+        paddingVertical: 10,
+        paddingHorizontal: 40,
+        marginTop: 20
+    },
+    textoBotao: {
+        color: 'white',
+        fontSize: 16,
+        fontWeight: 'bold'
+    }
+}
 
-AppRegistry.registerComponent(appName, () => App);
+// const botaoPressionado = () => {
+
+// }
+
+const App = () => {
+    const { principal, botao, textoBotao } = Estilos;
+
+    return (
+        <View style={principal}>
+            <Image
+                source={require('./imgs/logo.png')}
+            />
+            <TouchableOpacity style={botao}>
+                <Text style={textoBotao}>Nova frase...</Text>
+            </TouchableOpacity>
+        </View>
+    );
+};
+
+AppRegistry.registerComponent('frasesdodia', () => App);
+
