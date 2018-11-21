54854df80a05a5e5a7a00fbf7bd35ff0191bd0c2
Daniel Souza <danielfss98@gmail.com>
2018-11-21 16:24:49 -0200

    Criando uma lista de item

--- catalogo/src/componets/ListaItens.js
+++ catalogo/src/componets/ListaItens.js
@@ -1,22 +1,15 @@
 import React, { Component } from "react";
-import { AppRegistry, StyleSheet, Text, View } from "react-native";
+import { View } from "react-native";
+import Item from "../componets/Item";
 
 export default class catologo extends Component {
-  constructor(props) {
-    super(props);
-    console.log("Construindo a aplicação");
-  }
-
-  componentWillMount() {
-    console.log("Fazendo alguma coisa antes da renderização.");
-  }
-
-  componentDidMount() {
-    console.log("Fazer alguma coisa depois da renderização.");
-  }
-
   render() {
-    console.log("Objeto é renderizado");
-    return <Text>Testando</Text>;
+    return (
+      <View>
+        <Item />
+        <Item />
+        <Item />
+      </View>
+    );
   }
 }
