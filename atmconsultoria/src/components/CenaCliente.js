2a2b97e349e0e88e93c0d6363027698d5ff46466
Daniel Souza <danielfss98@gmail.com>
2018-11-26 12:43:15 -0200

    Definindo estilo cena cliente

--- atmconsultoria/src/components/CenaCliente.js
+++ atmconsultoria/src/components/CenaCliente.js
@@ -1,25 +1,62 @@
 import React, { Component } from "react";
-import { View, StatusBar, Image, Text } from "react-native";
+import { View, StatusBar, Image, Text, StyleSheet } from "react-native";
 import BarraNavegacao from "./BarraNavegacao";
 
 const detalheCliente = require("../../imgs/detalhe_cliente.png");
 const cliente1 = require("../../imgs/cliente1.png");
 const cliente2 = require("../../imgs/cliente2.png");
+const btnVoltar = require("../../imgs/btn_voltar.png");
 
 export default class CenaCliente extends Component {
   render() {
     return (
       <View>
-        <StatusBar backgroundColor="#CCC" />
-        <BarraNavegacao />
-    
-        <Image source={detalheCliente} />
-        <Text>Nossos clientes</Text>
-        <Image source={cliente1} />
-        <Text>Nossos clientes</Text>
-        <Image source={cliente2} />
-        <Text>Nossos clientes</Text>
+        <StatusBar backgroundColor="#B9C941" />
+        <View style={styles.barraNavegacao}>
+          <Image style={styles.btnVoltar} source={btnVoltar} />
+          <BarraNavegacao />
+        </View>
+
+        <View style={styles.cabecalho}>
+          <Image source={detalheCliente} />
+          <Text style={styles.txtNossosClientes}>Nossos clientes</Text>
+        </View>
+        <View style={styles.infoCliente}>
+          <Image source={cliente1} />
+          <Text style={styles.txtInfoCliente}>Nossos clientes</Text>
+        </View>
+        <View style={styles.infoCliente}>
+          <Image source={cliente2} />
+          <Text style={styles.txtInfoCliente}> Nossos clientes</Text>
+        </View>
       </View>
     );
   }
 }
+
+const styles = StyleSheet.create({
+  barraNavegacao: {
+    flexDirection: "row",
+    backgroundColor: "#B9C941"
+  },
+  btnVoltar: {
+    margin: 10
+  },
+  cabecalho: {
+    flexDirection: "row",
+    marginTop: 15
+  },
+  txtNossosClientes: {
+    fontSize: 30,
+    color: "#B9C941",
+    marginTop: 20
+  },
+  infoCliente: {
+    margin: 20
+  },
+  txtInfoCliente: {
+    fontSize: 18,
+    color: "#000",
+    marginLeft: 20
+  }
+});
