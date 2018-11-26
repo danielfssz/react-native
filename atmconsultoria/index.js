219156350d20819f43b4b22246930cc07a0208e7
Daniel Souza <danielfss98@gmail.com>
2018-11-26 11:51:40 -0200

    add Barra de navegação

--- atmconsultoria/index.js
+++ atmconsultoria/index.js
@@ -1,7 +1,16 @@
-/** @format */
+import React, { Component } from "react";
+import { AppRegistry, View } from "react-native";
 
-import {AppRegistry} from 'react-native';
-import App from './App';
-import {name as appName} from './app.json';
+import BarraNavegacao from "./src/components/BarraNavegacao";
 
-AppRegistry.registerComponent(appName, () => App);
+export default class atmconsultoria extends Component {
+  render() {
+    return (
+      <View>
+        <BarraNavegacao />
+      </View>
+    );
+  }
+}
+
+AppRegistry.registerComponent("atmconsultoria", () => atmconsultoria);
