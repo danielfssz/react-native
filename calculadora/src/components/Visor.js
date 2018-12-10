64f766782a8360b1d23846179ee9e700a965aeff
Daniel Souza <danielfss98@gmail.com>
2018-12-10 13:50:21 -0200

    Componente visor ok

---
+++ calculadora/src/components/Visor.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { TextInput, View, StyleSheet } from 'react-native';
+
+export default props => (
+  <View>
+    <TextInput style={styles.visor} placeholder="Resultado" editable={false} />
+  </View>
+);
+
+const styles = StyleSheet.create({
+  visor: {
+    height: 100,
+    fontSize: 30
+  }
+});
