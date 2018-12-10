930975cb2cc9b6cd9e5dae6af1c97e618910a936
Daniel Souza <danielfss98@gmail.com>
2018-12-10 14:31:06 -0200

    Add botao de calcular

--- calculadora/src/components/Comando.js
+++ calculadora/src/components/Comando.js
@@ -1,8 +1,6 @@
 import React from 'react';
-import { Text, View } from 'react-native';
+import { Button, View } from 'react-native';
 
 export default props => (
-  <View>
-    <Text>Comando</Text>
-  </View>
+      <Button title='Calcular' />
 );
